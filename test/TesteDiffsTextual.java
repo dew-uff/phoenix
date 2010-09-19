0c0edfd495e593702421345c89f94404e4adc0c2
brunopinto <brunopinto@3a7cf50b-9217-471b-a0fb-ae99d89c214b>
2010-09-19 23:11:32 +0000

    * Removidas as bibliotecas adicionadas manualmente. * Adicionada o JUnit que vem junto do Netbeans.

--- test/TesteDiffsTextual.java
+++ test/TesteDiffsTextual.java
@@ -1,35 +1,26 @@
 
 import controlador.Diffs;
-import java.util.Scanner;
 import org.junit.Test;
-import static org.testng.Assert.*;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+import static org.junit.Assert.*;
 
 /**
  *
  * @author campello
  */
-
 public class TesteDiffsTextual {
 
     String linha1 = "ceu e azul";
-    String linha2= "ceu e roxo";
-
-
+    String linha2 = "ceu e roxo";
 
     @Test
-    public void testeSimilaridadeStringsIgualsRetornandoSeuPercentual(){
-        Diffs diffs =  new Diffs();
-        assertEquals(diffs.diffTextual3(linha1,linha1),100);
+    public void testeSimilaridadeStringsIgualsRetornandoSeuPercentual() {
+        Diffs diffs = new Diffs();
+        assertEquals(diffs.diffTextual3(linha1, linha1), 100);
     }
+
     @Test
-    public void testeSimilaridadeStringsDifRetornandoSeuPercentual(){
-        Diffs diffs =  new Diffs();
-        assertEquals(diffs.diffTextual3(linha1,linha2),50);
+    public void testeSimilaridadeStringsDifRetornandoSeuPercentual() {
+        Diffs diffs = new Diffs();
+        assertEquals(diffs.diffTextual3(linha1, linha2), 50);
     }
-   
 }
