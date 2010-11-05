eb47cea5ac740bddb1f734fbb0d2ccf4a5103888
brunopinto <brunopinto@3a7cf50b-9217-471b-a0fb-ae99d89c214b>
2010-11-05 02:10:20 +0000

    * Adicionado o Mockito. * Inicio do tratamento dos atributos de um elemento.

--- src/modelo/SimilarNode.java
+++ src/modelo/SimilarNode.java
@@ -1,9 +1,11 @@
 package modelo;
 
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
 public class SimilarNode extends Similar<SimilarNode> {
 
+    public static final double ATTRIBUTE_WEIGTH = 0.5;
     private Node node;
 
     public SimilarNode(Node node) {
@@ -13,11 +15,22 @@ public class SimilarNode extends Similar<SimilarNode> {
     @Override
     public double similar(SimilarNode y) {
         Node otherNode = y.getNode();
-        
+
         if (node.getNodeName().equals(otherNode.getNodeName())) {
-            return 1;
+            if (node.hasAttributes() || otherNode.hasAttributes()) {
+                if (node.hasAttributes() && otherNode.hasAttributes()) {
+                    NamedNodeMap attributesFromNode = node.getAttributes();
+                    NamedNodeMap attributesFromOtherNode = otherNode.getAttributes();
+                    
+                    return 1;
+                } else {
+                    return 0.5;
+                }
+            } else {
+                return 1;
+            }
         } else {
-            return 0.1;
+            return 0;
         }
     }
 
