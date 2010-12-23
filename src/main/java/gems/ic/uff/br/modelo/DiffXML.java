05e81ea749f00288f9f66f7860a6b90b23490abd
campello <campello@3a7cf50b-9217-471b-a0fb-ae99d89c214b>
2010-12-23 02:37:28 +0000

    colocando Maven no projeto

---
+++ src/main/java/gems/ic/uff/br/modelo/DiffXML.java
@@ -0,0 +1,14 @@
+package gems.ic.uff.br.modelo;
+
+public class DiffXML {
+
+    public static double calcularPercentual(String texto1, String texto2) {
+        int qtdCaracteresIguais = 0;
+
+        if (texto1.equals(texto2)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/gems/ic/uff/br/modelo/LcsXML.java
@@ -0,0 +1,32 @@
+package gems.ic.uff.br.modelo;
+
+public class LcsXML extends LongestCommonSubsequence<SimilarNode> {
+
+    private XML x;
+    private XML y;
+
+    public LcsXML(XML from, XML to) {
+        this.x = from;
+        this.y = to;
+    }
+
+    @Override
+    protected int lengthOfX() {
+        return x.getDocument().getDocumentElement().getChildNodes().getLength();
+    }
+
+    @Override
+    protected int lengthOfY() {
+        return y.getDocument().getDocumentElement().getChildNodes().getLength();
+    }
+
+    @Override
+    protected SimilarNode valueOfX(int index) {
+        return new SimilarNode(x.getDocument().getDocumentElement().getChildNodes().item(index));
+    }
+
+    @Override
+    protected SimilarNode valueOfY(int index) {
+        return new SimilarNode(y.getDocument().getDocumentElement().getChildNodes().item(index));
+    }
+}
