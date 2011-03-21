296ebfb14df86ece530507adb9e723726565e2f6
campello <campello@3a7cf50b-9217-471b-a0fb-ae99d89c214b>
2011-03-21 00:14:47 +0000

    Criando uma classe para adicionar os dados estatisticos de similaridade.

---
+++ phoenix/src/test/java/gems/ic/uff/br/modelo/DadosEstatisticos.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gems.ic.uff.br.modelo;
+
+/**
+ *
+ * @author campello
+ */
+public class DadosEstatisticos {
+
+    private String node1;
+    private String node2;
+    private Double percentual;
+
+    /**
+     * @return the node1
+     */
+    public String getNode1() {
+        return node1;
+    }
+
+    /**
+     * @param node1 the node1 to set
+     */
+    public void setNode1(String node1) {
+        this.node1 = node1;
+    }
+
+    /**
+     * @return the node2
+     */
+    public String getNode2() {
+        return node2;
+    }
+
+    /**
+     * @param node2 the node2 to set
+     */
+    public void setNode2(String node2) {
+        this.node2 = node2;
+    }
+
+    /**
+     * @return the percentual
+     */
+    public Double getPercentual() {
+        return percentual;
+    }
+
+    /**
+     * @param percentual the percentual to set
+     */
+    public void setPercentual(Double percentual) {
+        this.percentual = percentual;
+    }
+
+}
