b4b7d4c5cf39e3f235d198d7d25ccb67de0fdc9e
campello <campello@3a7cf50b-9217-471b-a0fb-ae99d89c214b>
2011-03-21 15:24:33 +0000

    retirando testes desnecessarios

--- phoenix/src/test/java/gems/ic/uff/br/modelo/SimilarNodeTest.java
+++ phoenix/src/test/java/gems/ic/uff/br/modelo/SimilarNodeTest.java
@@ -154,20 +154,26 @@ public class SimilarNodeTest {
         assertEquals(1 - SimilarNode.ELEMENT_CHILDREN_WEIGTH, similarNode.elementsChildrenSimilarity(similarNode2.getNode(), 1), 0);
     }
 
+    @Test
     public void testeBobo() {
-        MongeElkan me = new MongeElkan();
-        System.out.println("TOTALLLLL =>  " + me.getSimilarity("carta", "elemento1"));
+        List<String> lista1 = new ArrayList<String>();
+        List<String> lista2 = new ArrayList<String>();
+        lista1.add("a");
+        lista1.add("b");
+        lista2.add("c");
+        lista2.addAll(lista1);
+        System.out.println("LISTAAAAAAAA    " + lista2);
+
     }
 
 //    @Test
-    @Test
     public void similaridadeDeconjuntosComDoisXmlDeMesmoTamanho() throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
-        Document xml1 = db.parse(new InputSource(new StringReader("<raiz><elemento1/><porta/><carta/></raiz>")));
-        Document xml2 = db.parse(new InputSource(new StringReader("<raiz><torta/><pata/><elemento1/></raiz>")));
-//        Document xml1 = db.parse(new InputSource(new StringReader("<raiz><elemento1/><elemento2/><elemento3/></raiz>")));
-//        Document xml2 = db.parse(new InputSource(new StringReader("<raiz><elemento3/><elem1/><elemento2/></raiz>")));
+//        Document xml1 = db.parse(new InputSource(new StringReader("<raiz><elemento1/><porta/><carta/></raiz>")));
+//        Document xml2 = db.parse(new InputSource(new StringReader("<raiz><torta/><pata/><elemento1/></raiz>")));
+        Document xml1 = db.parse(new InputSource(new StringReader("<raiz><elemento1/><elemento2/><elemento3/></raiz>")));
+        Document xml2 = db.parse(new InputSource(new StringReader("<raiz><elemento3/><elemento1/><elemento2/></raiz>")));
 
         NodeList listaXml1 = xml1.getElementsByTagName("*");
         NodeList listaXml2 = xml2.getElementsByTagName("*");
@@ -186,6 +192,29 @@ public class SimilarNodeTest {
 
 //        System.out.println("TODOSSSSS OS VALORESSSSS        " + procurarSimilaridades(listaXml1.item(0).getChildNodes(), listaXml2.item(0).getChildNodes(), resultadoGeral));
 //        System.out.println("TOTAL DE SIMILARIDADES ENCONTRADAS     " + qtdSimilaridadesEncontradas);
+    }
+
+    @Test
+    public void similaridadeDeconjuntosComDoisXmlComMesmosElementosMasTamanhoDiferentes() throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document xml1 = db.parse(new InputSource(new StringReader("<raiz><a/><a/><a/><b/><b/><c/></raiz>")));
+        Document xml2 = db.parse(new InputSource(new StringReader("<raiz><a/><b/><c/></raiz>")));
+
+        NodeList listaXml1 = xml1.getElementsByTagName("*");
+        NodeList listaXml2 = xml2.getElementsByTagName("*");
+        List<DadosEstatisticos> resultadoGeral = procurarSimilaridades(listaXml1.item(0).getChildNodes(), listaXml2.item(0).getChildNodes());
+        for (DadosEstatisticos dadosEstatisticos : resultadoGeral) {
+            System.out.printf("%s   <====>   %s  percentual de similaridade  = %.3f\n", dadosEstatisticos.getNode1(), dadosEstatisticos.getNode2(), dadosEstatisticos.getPercentual());
+        }
+        System.out.printf("\n\n");
+        System.out.println("SIMILARIDADES ENTRE NODES COM O PERCENTUAL ACIMA DO ESTABELECIDO:");
+        for (DadosEstatisticos dadosEstatisticos : resultadoGeral) {
+            if (dadosEstatisticos.getPercentual() > 0.7) {
+                System.out.printf("%s   <====>   %s  percentual de similaridade  = %.3f\n", dadosEstatisticos.getNode1(), dadosEstatisticos.getNode2(), dadosEstatisticos.getPercentual());
+
+            }
+        }
     }
 
     public void similaridadeConjuntosDoisXmlMesmoTamanhoComUmElementoAbaixoLimiteEstabelecido() throws ParserConfigurationException, SAXException, IOException {
@@ -216,105 +245,25 @@ public class SimilarNodeTest {
         assertEquals(0, similarNode.similar(similarNode2), 0);
     }
 
-//    @Test
-    public void similaridadeEntreConjuntos() {
-        try {
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document d = db.parse(new InputSource(new StringReader("<raiz><nome></nome><end><rua></rua><cidade></cidade></end></raiz>")));
-            Document d2 = db.parse(new InputSource(new StringReader("<raiz><CPF></CPF><end><rua></rua><cidade></cidade></end><nome></nome></raiz>")));
-
-            int contadorDeSimilaridade = 0;
-            NodeList nl = d.getElementsByTagName("*");
-            NodeList nl2 = d.getElementsByTagName("*");
-            if (nl.getLength() <= nl2.getLength()) {
-
-                for (int i = 0; i < nl.getLength(); i++) {
-                    NodeList noCorrente = d2.getElementsByTagName("" + nl.item(i).getNodeName());
-                    if (noCorrente != null) {
-                        contadorDeSimilaridade++;
-                    }
-                    noCorrente = null;
-                }
-            } else {
-                for (int i = 0; i < nl2.getLength(); i++) {
-                    NodeList noCorrente = d.getElementsByTagName("" + nl2.item(i).getNodeName());
-                    if (noCorrente == null) {
-                        contadorDeSimilaridade++;
-                    }
-                    noCorrente = null;
-                }
-            }
-            float x = nl.getLength();
-            System.out.println("PERCENTUAL DE SIMILARIDADE =>  " + contadorDeSimilaridade / x);
-
-
-//        SimilarNode similarNode = createSimilarNode("<raiz><nome></nome><CPF></CPF><end><rua></rua><cidade></cidade></end></raiz>");
-//        SimilarNode similarNode2 = createSimilarNode("<raiz><nome></nome><CPF></CPF><end><rua></rua><cidade></cidade></end></raiz>");
-//
-//        assertEquals(1, similarNode.elementsChildrenSimilarity(similarNode2.getNode(), 1), 0);
-            } catch (SAXException ex) {
-            Logger.getLogger(SimilarNodeTest.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(SimilarNodeTest.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParserConfigurationException ex) {
-            Logger.getLogger(SimilarNodeTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-//        SimilarNode similarNode = createSimilarNode("<raiz><nome></nome><CPF></CPF><end><rua></rua><cidade></cidade></end></raiz>");
-//        SimilarNode similarNode2 = createSimilarNode("<raiz><nome></nome><CPF></CPF><end><rua></rua><cidade></cidade></end></raiz>");
-//
-//        assertEquals(1, similarNode.elementsChildrenSimilarity(similarNode2.getNode(), 1), 0);
-    }
-
-//    @Test
-    public void imprimirTodosFilhos() throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        Document d = db.parse(new InputSource(new StringReader("<raiz><nome></nome><CPF></CPF><end><rua></rua><cidade></cidade></end></raiz>")));
-
-        NodeList nl = d.getElementsByTagName("*");
-        Node n = nl.item(0);
-        System.out.println("RAIZ ========== ");
-
-        System.out.println("" + n.getNodeName());
-        if (n.hasChildNodes()) {
-            imprimir(n.getFirstChild());
-        }
-        System.out.println("FIMMMMMMMMMMM");
-    }
-
-    private void imprimir(Node firstChild) {
-        System.out.println("PROXIMO NO ===========");
-        if (firstChild != null) {
-            System.out.println("" + firstChild.getNodeName());
-            System.out.println("PARENT =>   " + firstChild.getParentNode().getNodeName());
-            if (firstChild.hasChildNodes()) {
-                imprimir(firstChild.getFirstChild());
-            } else {
-                imprimir(firstChild.getNextSibling());
-            }
-
-        }
-    }
-
     private List<DadosEstatisticos> procurarSimilaridades(NodeList lista1, NodeList lista2) {
         double similaridade = 0.0;
         List<DadosEstatisticos> resultado = new ArrayList<DadosEstatisticos>();
+        List<String> listaNodesComparados = new ArrayList<String>();
         MongeElkan me = new MongeElkan();
         for (int i = 0; i < lista1.getLength(); i++) {
             for (int j = 0; j < lista2.getLength(); j++) {
 //                if (me.getSimilarity(lista1.item(i).getNodeName(), lista2.item(j).getNodeName()) > 0.7) {
 //                qtdSimilaridadesEncontradas++;
                 double similaridadeCorrente = me.getSimilarity(lista1.item(i).getNodeName(), lista2.item(j).getNodeName());
+                listaNodesComparados.add(lista1.item(i).getNodeName());
                 DadosEstatisticos dadosEstatisticos = new DadosEstatisticos();
                 dadosEstatisticos.setNode1(lista1.item(i).getNodeName());
                 dadosEstatisticos.setNode2(lista2.item(j).getNodeName());
                 dadosEstatisticos.setPercentual(similaridadeCorrente);
                 resultado.add(dadosEstatisticos);
                 similaridade += similaridadeCorrente;
-                if (lista1.item(i).hasChildNodes() && lista2.item(j).hasChildNodes()) {
+//                if (lista1.item(i).hasChildNodes() && lista2.item(j).hasChildNodes()) {
 //                    similaridade += procurarSimilaridades(lista1.item(i).getChildNodes(), lista2.item(j).getChildNodes());
-                }
 //                }
             }
         }
